a804542f4808fc084742dcb5e2eac966574c4557
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-18 23:56:11 +0200

    with button

--- src/pages/PageWelcome.ts
+++ src/pages/PageWelcome.ts
@@ -2,6 +2,7 @@ export const PageWelcome = () => {
 	return /*html*/`
 <div class="page pageWelcome">
 	<p class="text-red-500">Welcome to this site.</p>
+	<button class="btn btn-primary">Button</button>
 </div>
 `; 
 }
